7597f1c69db17237bb40ffe9b6019c7be43daf80
ashishsharma8094 <72527664+ashishsharma8094@users.noreply.github.com>
2021-05-27 02:47:00 +0530

    Add files via upload

---
+++ hitchkkers/Data.java
@@ -0,0 +1,19 @@
+package hitchkkers;
+import java.sql.*;
+
+/*
+@author Ashish sharma
+ */
+public class Data{
+    Connection c;
+    Statement s;
+    public Data(){
+        try{
+           Class.forName("com.mysql.jdbc.Driver");
+           c = DriverManager.getConnection("jdbc:mysql://localhost:3306/hitchkkers","root","8094@Mnnit");
+           s=c.createStatement();
+        }catch(Exception e){
+            
+        }
+    }
+}
